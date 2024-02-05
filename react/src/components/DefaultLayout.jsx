184e0002225dc681ae1417276762191ea71d663a
Einswald <poopystink77@gmail.com>
2024-02-05 15:25:46 +0700

    Menambahkan Sidebar - Ridwan

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,16 +1,32 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useStateContext } from "../contexts/ContextProvider";
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { Link } from 'react-router-dom';
 
-export default function DefaultLayout() {
-    const { user, token } = useStateContext()
-
-    if (!token) {
-        return <Navigate to="/login" />
-    }
-
+export default function CustomSidebar() {
     return (
-        <div>
-            <Outlet />
-        </div>
-    )
+        <Sidebar className="w-64 h-screen bg-blue-700 p-4">
+            <Menu
+                menuItemStyles={{
+                    button: {
+                        // the active class will be added automatically by react router
+                        // so we can use it to style the active menu item
+                        [`&.active`]: {
+                            backgroundColor: '#13395e',
+                            color: '#b6c8d9',
+                        },
+                    },
+                }}
+            >
+                <MenuItem>
+                    <Link to="/dashboard" className="block py-2 px-4 rounded-md text-orange-500 no-underline ease-in-out duration-200">
+                        Dashboard
+                    </Link>
+                </MenuItem>
+                <MenuItem>
+                    <Link to="/users" className="block py-2 px-4 rounded-md text-orange-500 no-underline ease-in-out duration-200">
+                        Users
+                    </Link>
+                </MenuItem>
+            </Menu>
+        </Sidebar>
+    );
 }
